a32b13c6ad9a02b5d599330b2ac5ffc05ccecdf8
Kosuke Takeda <kosuke.takeda@cern.ch>
2019-09-24 17:15:03 +0200

    Succeeded to communicate with the MySQL data base.

--- color.js
+++ color.js
@@ -1,55 +1,81 @@
 $(function() {
-  $('#btn-test1').on("click", function() {
-    var japan_map = document.getElementById("japan_map");
-    if ( !japan_map ) {
-      alert("SVG file ID was not found.");
-    }
-    var svgDoc = japan_map.contentDocument;
-    alert(svgDoc);
-    //var $svg = $(svgDoc).find('svg');
-    //alert(console.dir($svg));
-//    var $svg = $(svgDoc).find("svg");
-////    var $svg = $(svg);
-//    alert('svg ' + $svg.length );
+    $('#btn-test1').on("click", function() {
+        var japan_map = document.getElementById("japan_map");
+        if ( !japan_map ) {
+            alert("SVG file ID was not found.");
+        }
+        var svgDoc = japan_map.contentDocument;
+        alert(svgDoc);
+        //var $svg = $(svgDoc).find('svg');
+        //alert(console.dir($svg));
+        //    var $svg = $(svgDoc).find("svg");
+        ////    var $svg = $(svg);
+        //    alert('svg ' + $svg.length );
 
-   // if ( !map ) {
-   //   alert('SVG map is null : ' + map);
-   // }
-   // $prefecture = map.find('#HOKKAIDO');
-   // alert('line9 : '+$prefecture.length);
-   // $map = $(map);
-   // alert($map.length);
-   // //$prefecture = $('#HOKKAIDO', $map);
-   // //$prefecture = $map.find('path');
-   // $prefecture = $map.find('#HOKKAIDO');
-   // //    $prefecture = $('#main').find('#btn-test1'); --> lenght = 1
-   // alert($prefecture.length);
-   // $prefecture.css('fill', '#ff0000');
-   // //    $prefecture.find('path').attr('fill', '#ff0000');
+        // if ( !map ) {
+        //   alert('SVG map is null : ' + map);
+        // }
+        // $prefecture = map.find('#HOKKAIDO');
+        // alert('line9 : '+$prefecture.length);
+        // $map = $(map);
+        // alert($map.length);
+        // //$prefecture = $('#HOKKAIDO', $map);
+        // //$prefecture = $map.find('path');
+        // $prefecture = $map.find('#HOKKAIDO');
+        // //    $prefecture = $('#main').find('#btn-test1'); --> lenght = 1
+        // alert($prefecture.length);
+        // $prefecture.css('fill', '#ff0000');
+        // //    $prefecture.find('path').attr('fill', '#ff0000');
 
-   // //var $prefecture = $map.find('#HOKKAIDO'); 
-   // //$prefecture.css('fill', '#ff0000');
-  });
+        // //var $prefecture = $map.find('#HOKKAIDO'); 
+        // //$prefecture.css('fill', '#ff0000');
+    });
 
-  $('#btn-test2').on("click", function() {
-    var japan_map = document.getElementById("japan_map").contentDocument;
-    var $japan_map = $(japan_map);
-    if ( !japan_map ) {
-      alert("SVG file ID was not found.");
-    }
-    var list = ["#HOKKAIDO", "#IWATE"];
-    for ( var index = 0; index < list.length; index++ ){
-        var $prefecture = $japan_map.find(list[index]);
-        $prefecture.css("fill", "#0000ff");
-    }
-  });
+    $('#btn-test2').on("click", function() {
+        var japan_map = document.getElementById("japan_map").contentDocument;
+        var $japan_map = $(japan_map);
+        if ( !japan_map ) {
+            alert("SVG file ID was not found.");
+        }
+        var list = ["#HOKKAIDO", "#IWATE", "#MIYAGI", "", "", "", "", "",];
+        for ( var index = 0; index < list.length; index++ ){
+            var $prefecture = $japan_map.find(list[index]);
+            $prefecture.css("fill", "#0000ff");
+        }
+    });
 
-  $('#btn-test3').on("click", function() {
-    $('#path3').css({ fill: "#0000ff" });
-  });
+    $('#btn-test3').on("click", function() {
+        $('#path3').css({ fill: "#0000ff" });
+    });
 
-  $('#btn-reset').on("click", function() {
-    $('#path1, #path2, #path3').css({ fill: "#ffffff" });
-  });
+    $('#btn-reset').on("click", function() {
+        $('#path1, #path2, #path3').css({ fill: "#ffffff" });
+    });
+
+    $('#btn-search').on("click", function() {
+
+        // receive data from HTML
+        var data = { request : $('#request').val()};
+        alert($('#request').val())
+
+        // Ajax 
+        $.ajax({
+            type: "POST", // POST
+            url: "./mysql_map.php", // Define the destination URL
+            data: data,
+
+            // if the process succeed
+            success : function(data, dataType) {
+                // add the response data to HTML file 
+                $('#res').html(data);
+            },
+
+            // error
+            error : function(XMLHttpRequest, textStatus, errorThrown) {
+                alert("Failed: XMLHttpRequest : " + XMLHttpRequest.status + " txtStatus : " + textStatus + " errorThrown : " + errorThrown.message); 
+            },
+        });
+    return false;
+    });
 
 });
---
+++ mysql_map.php
@@ -0,0 +1,48 @@
+<?php
+
+// receive post data 
+$prefecture = $_POST['request'];
+
+// not empty
+if (!empty($prefecture)) {
+
+    // pdo instance
+    $dsn = "mysql:dbname=TownWork;host=dbod-twdb.cern.ch;port=5509";
+    $user = "admin";
+    $password = "townwork";
+    $pdo;
+    
+    try {
+        $pdo = new PDO($dsn, $user, $password);
+    } catch (PDOException $e) {
+        echo "Failed: " . $e->getMessage() . "\n";
+    }
+    
+    // SQL cuery
+    //    $sql = "select city from local.test where prefecture = '".$prefecture."'";
+    $results = $pdo->query("select database();");
+    $test = $results->fetch(PDO::FETCH_NUM);
+    echo $test[0];
+
+    $sql = "select * from info;";
+    $results = $pdo->query($sql);
+
+    // Output html 
+    echo '<table class="list_table">';
+    echo "<tr>";
+    echo "<th>市町村</th>";
+    echo "</tr>";
+    foreach ($results as $result) {
+        echo "<tr>";
+        echo "<td>".$result['id']."</td>";
+        echo "<td>".$result['name']."</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+
+    //空だったら
+} else {
+    echo '<p id="tekito">エラー：都道府県を選択して下さい。</p>';
+}
+
+?>
